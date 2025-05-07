2553c54fe8a8707cee8032f2a90ae4691eb51413
UCHIHAPATRIC <patricmorales1983@gmail.com>
2025-05-07 11:05:59 -0300

    Update pessoa.py

--- pessoa.py
+++ pessoa.py
@@ -1,14 +1,20 @@
 from abc import ABC, abstractmethod
 
 class Pessoa(ABC):
+    """
+    Classe abstrata que representa uma pessoa.
+    """
     def __init__(self, id: int, nome: str, cpf: str):
-        self.id = id
-        self.nome = nome
-        self._cpf = cpf  
+        self.id: int = id
+        self.nome: str = nome
+        self._cpf: str = cpf 
 
     @abstractmethod
-    def marcarPresenca(self):
+    def marcar_presenca(self) -> None:
+        """
+        Método abstrato para marcar presença.
+        """
         pass
 
-    def matricular(self):
-        print(f"{self.nome} foi matriculado.")
+    def matricular(self) -> None:
+        print(f"{self.nome} foi matriculado com sucesso.")
